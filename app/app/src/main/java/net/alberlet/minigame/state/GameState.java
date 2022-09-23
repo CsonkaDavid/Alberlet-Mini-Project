36259d31b29f82fdaebc2f95c9a422d7a1e95bee
Broxigar11 <karafadavid11@gmail.com>
2022-09-24 00:04:26 +0200

    Tablafeltolto algoritmus elkeszitese

--- app/app/src/main/java/net/alberlet/minigame/state/GameState.java
+++ app/app/src/main/java/net/alberlet/minigame/state/GameState.java
@@ -10,13 +10,42 @@ public class GameState {
 
     private int dice;
 
+    private Player currentPlayer;
+
     public GameState() {
+        currentPlayer = PLAYER_ONE;
         rollDice();
+
+    }
+
+    private void startTurn() {
+        if(currentPlayer.equals(PLAYER_ONE)) {
+            currentPlayer = PLAYER_TWO;
+        } else {
+            currentPlayer = PLAYER_ONE;
+        }
+        rollDice();
+
     }
 
     private void rollDice() {
         Random random = new Random();
         dice = random.nextInt(6) + 1;
+    }
+
+    public void addValueToTableOnColumn(int value, int column) throws UnsupportedOperationException{
+        if(column < 1 || column > 3) {
+            throw new UnsupportedOperationException("Az oszlop indexe nem 1 es 3 kozott van!");
+        }
+
+        if(column == 1) {
+            currentPlayer.getTable().addFirstColumn(value);
+        } else if (column == 2) {
+            currentPlayer.getTable().addSecondColumn(value);
+        } else if (column == 3) {
+            currentPlayer.getTable().addThirdColumn(value);
+        }
+
     }
 
 }

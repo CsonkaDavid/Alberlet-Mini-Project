495b1763f7904ca56e9203f84fa7649c132a2d5d
l1ng4n27 <71123447+l1ng4n27@users.noreply.github.com>
2022-09-24 18:43:28 +0200

    GameController osztály konstruktor és a játékos nevét lekérő metódus elkészítése

--- app/app/src/main/java/net/alberlet/minigame/gui/GameController.java
+++ app/app/src/main/java/net/alberlet/minigame/gui/GameController.java
@@ -1,5 +1,24 @@
 package net.alberlet.minigame.gui;
 
+import net.alberlet.minigame.state.GameState;
+
 public class GameController {
+    private static final GameState GAME_STATE = new GameState();
+
+    public GameController(){
+
+    }
+
+    public void startNewTurn(){
+        GAME_STATE.startTurn();
+    }
+
+    public String getCurrentPlayerName(){
+
+        return GAME_STATE.getActivePlayer().toString();
+
+    }
+
+
 
 }

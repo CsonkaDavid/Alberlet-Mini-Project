8d5f7929551323128e713070eb65884d7845568b
CsonkaDavid <shay9505@gmail.com>
2022-09-24 13:56:48 +0200

    Pontszám összeszámláló függvények hozzáadása Player osztályhoz

--- app/app/src/main/java/net/alberlet/minigame/state/Player.java
+++ app/app/src/main/java/net/alberlet/minigame/state/Player.java
@@ -1,5 +1,7 @@
 package net.alberlet.minigame.state;
 
+import java.util.List;
+
 public class Player {
 
     private final Table table;
@@ -23,4 +25,51 @@ public class Player {
         this.score = score;
     }
 
+    public int sumOfScores() {
+        int score = 0;
+
+        score += sumOfScoresInTableColumn(table.getFirstColumn());
+        score += sumOfScoresInTableColumn(table.getSecondColumn());
+        score += sumOfScoresInTableColumn(table.getThirdColumn());
+
+        return score;
+    }
+
+    private int sumOfScoresInTableColumn(List<Integer> column) {
+        int score = 0;
+        int value;
+        int savedValue = 0;
+
+        for(int i = 0; i < 3; i++) {
+            value = column.get(i);
+
+            if(value == savedValue)
+                continue;
+
+            int counter = 1;
+
+            for(int j = i+1; j < 3; j++)
+                if (column.get(j) == value)
+                    counter++;
+
+            switch (counter) {
+                case 1: {
+                    score += value;
+                    break;
+                }
+                case 2: {
+                    score += value * 4;
+                    savedValue = value;
+                    break;
+                }
+                case 3: {
+                    score += value * 9;
+                    i = counter;
+                    break;
+                }
+            }
+        }
+
+        return score;
+    }
 }

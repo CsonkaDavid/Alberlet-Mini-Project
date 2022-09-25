75667b278d513017cbdce31f1d46d610b2d59ed2
l1ng4n27 <71123447+l1ng4n27@users.noreply.github.com>
2022-09-25 19:13:29 +0200

    Felhasznált View-k megtalálása és elmentése változókba

--- app/app/src/main/java/net/alberlet/minigame/gui/GameActivity.java
+++ app/app/src/main/java/net/alberlet/minigame/gui/GameActivity.java
@@ -2,6 +2,8 @@ package net.alberlet.minigame.gui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +11,38 @@ import net.alberlet.minigame.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    TextView turnDisplay;
+
+    ImageView dice;
+
+    View player1Table;
+
+    View player2Table;
+
+    TextView player1OwnScore;
+
+    TextView player1OpponentScore;
+
+    TextView player2OwnScore;
+
+    TextView player2OpponentScore;
+
     private static final GameController GAME_CONTROLLER = new GameController();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
+
+        turnDisplay = findViewById(R.id.turn_display);
+        dice = findViewById(R.id.dice);
+        player1Table = findViewById(R.id.player1_table);
+        player2Table = findViewById(R.id.player2_table);
+        player1OwnScore = findViewById(R.id.player1_own_score);
+        player1OpponentScore = findViewById(R.id.player1_opponent_score);
+        player2OwnScore = findViewById(R.id.player2_own_score);
+        player2OpponentScore = findViewById(R.id.player2_opponent_score);
+
+
 
     }
 
